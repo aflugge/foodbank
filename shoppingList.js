9526fdc30b8fc6f42cd576fe5fa45d37e531e8db
Anton Flugge <anton.fluegge@gmx.de>
2021-04-09 10:25:52 +0100

    initial commit

---
+++ shoppingList.js
@@ -0,0 +1,12 @@
+neededItems = [
+  {'name':'Tinned meat', 'weight':100},
+  {'name':'Jam', 'weight':20},
+  {'name':'Coffee', 'weight':30},
+  {'name':'Tinned fruit', 'weight':50},
+  {'name':'Tinned deserts', 'weight':40},
+  {'name':'Sugar', 'weight':20},
+  {'name':'Long-life milk', 'weight':60},
+  {'name':'Biscuits', 'weight':10},
+  {'name':'Pasta, if you must', 'weight':0},
+  {'name':'Ok, tinned tomatoes', 'weight':0}
+]
