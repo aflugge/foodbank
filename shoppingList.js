07299df333edd5375be4c826d370173ae7dc669d
Anton Flugge <anton.fluegge@gmx.de>
2021-04-09 11:29:31 +0100

    added images

--- shoppingList.js
+++ shoppingList.js
@@ -1,12 +1,12 @@
 neededItems = [
-  {'name':'Tinned meat', 'weight':100},
-  {'name':'Jam', 'weight':20},
-  {'name':'Coffee', 'weight':30},
-  {'name':'Tinned fruit', 'weight':50},
-  {'name':'Tinned deserts', 'weight':40},
-  {'name':'Sugar', 'weight':20},
-  {'name':'Long-life milk', 'weight':60},
-  {'name':'Biscuits', 'weight':10},
-  {'name':'Pasta, if you must', 'weight':0},
-  {'name':'Ok, tinned tomatoes', 'weight':0}
+  {'name':'Tinned meat', 'weight':100, 'image':'meat.jpg'},
+  {'name':'Jam', 'weight':20, 'image':'jam.jpg'},
+  {'name':'Coffee', 'weight':30, 'image':'coffee.jpg'},
+  {'name':'Tinned fruit', 'weight':50, 'image':'fruit.jpg'},
+  {'name':'Tinned deserts', 'weight':40, 'image':'dessert.jpg'},
+  {'name':'Sugar', 'weight':20, 'image':'sugar.jpg'},
+  {'name':'Long-life milk', 'weight':60, 'image':'milk.jpg'},
+  {'name':'Biscuits', 'weight':10, 'image':'biscuits.jpg'},
+  {'name':'Pasta, if you must', 'weight':0, 'image':'pasta.jpg'},
+  {'name':'Ok, tinned tomatoes', 'weight':0, 'image':'tomatoes.jpg'}
 ]
